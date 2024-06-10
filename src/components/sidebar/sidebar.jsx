276199143a9fa2f2a03dc2adcc756a7e83b3465c
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2024-06-09 21:46:14 -0500

    remove sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,12 +1,4 @@
 import React, { Component } from "react";
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from "cdbreact";
 import { NavLink } from "react-router-dom";
 import "./sidebar.css";
 import logo from "../../img/logo.png";
@@ -44,9 +36,9 @@ class Sidebar extends Component {
     });
   };
 
-  clickSideBar = () =>{
+  clickSideBar = () => {
     this.props.clickSideBar();
-  }
+  };
 
   render() {
     return (
@@ -57,86 +49,7 @@ class Sidebar extends Component {
             height: "100%",
             overflow: "scroll initial",
           }}
-        >
-          <CDBSidebar textColor="#fff" backgroundColor="#545871">
-            <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"  onClick={this.clickSideBar}></i>}>
-              <a
-                href="/"
-                className="text-decoration-none"
-                style={{ color: "inherit" }}
-               
-              >
-                ReqBot
-              </a>
-            </CDBSidebarHeader>
-
-            <CDBSidebarContent className="sidebar-content">
-              <CDBSidebarMenu>
-                <CDBSidebarMenuItem className="center">
-                  <div>
-                    <img
-                      class="imgTable"
-                      src={logo}
-                      alt=" "
-                      onClick={this.goHome}
-                    />
-                  </div>
-                </CDBSidebarMenuItem>
-
-                <NavLink exact to="/dashboard/proyects">
-                  <CDBSidebarMenuItem className="titleSide">
-                    Proyectos
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {sessionStorage.getItem("rol") == "Administrador" ? (
-                  <NavLink exact to="/dashboard/organization">
-                    <CDBSidebarMenuItem className="titleSide">
-                      Organización
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-                ) : (
-                  <div></div>
-                )}
-
-                <NavLink exact to="/dashboard/logs">
-                  <CDBSidebarMenuItem className="titleSide">
-                    Logs
-                  </CDBSidebarMenuItem>
-                </NavLink>
-                <NavLink exact to="/dashboard/tickets">
-                  <CDBSidebarMenuItem className="titleSide">
-                    Tickets
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {sessionStorage.getItem("rol") == "Cliente" ? (
-                  <CDBSidebarMenuItem
-                    active="true"
-                    className="buttonSideBar"
-                    onClick={this.showAlert}
-                  >
-                    Redactar Requisito
-                  </CDBSidebarMenuItem>
-                ) : (
-                  <div></div>
-                )}
-              </CDBSidebarMenu>
-            </CDBSidebarContent>
-
-            <CDBSidebarFooter style={{ textAlign: "center" }}>
-              <div
-                style={{
-                  padding: "20px 5px",
-                }}
-                onClick={this.logout}
-                class="logout"
-              >
-                Cerrar Sesión
-              </div>
-            </CDBSidebarFooter>
-          </CDBSidebar>
-        </div>
+        ></div>
       </React.Fragment>
     );
   }
